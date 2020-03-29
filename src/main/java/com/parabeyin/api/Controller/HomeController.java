b40a48c5c67f71f5e336cae1d76ce88a0409ac4a
farhadmisirli <farhadmisirli@gmail.com>
2020-03-29 14:31:24 +0400

    HomeController created

--- src/main/java/com/parabeyin/api/Controller/HomeController.java
+++ src/main/java/com/parabeyin/api/Controller/HomeController.java
@@ -1,5 +1,18 @@
 package com.parabeyin.api.Controller;
 /* Created by Farhad on 2020-03-29 */
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
 public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Hello world");
+    }
+
 }
